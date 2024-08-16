95871f1a8270466dccecd78dea24a1fcbb0eaf83
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-08-16 11:31:23 +0300

    Add remove method

--- script.js
+++ script.js
@@ -16,13 +16,12 @@ class Tree {
     if (start > end) {
       return null;
     }
-    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    // const sortedArray = [...new Set(array)].sort((a, b) => a - b);
 
-    console.log(sortedArray);
     const mid = Math.floor((start + end) / 2);
-    const node = new Node(sortedArray[mid]);
-    node.left = this.buildTree(sortedArray, start, mid - 1);
-    node.right = this.buildTree(sortedArray, mid + 1, end);
+    const node = new Node(array[mid]);
+    node.left = this.buildTree(array, start, mid - 1);
+    node.right = this.buildTree(array, mid + 1, end);
 
     return node;
   }
@@ -48,12 +47,75 @@ class Tree {
     }
   }
 
+  remove(value) {
+    let node = this.find(value);
+    let hasLeftChild = node.left;
+    let hasRightChild = node.right;
+    let parentNode = this.findParent(node.data);
+
+    //remove when a node has two children
+    if (hasLeftChild && hasRightChild) {
+      const successor = this.findSuccessor(node);
+      const tempValue = node.data;
+      node.data = successor.data;
+      successor.data = tempValue;
+      node = successor;
+      hasLeftChild = node.left;
+      hasRightChild = node.right;
+      parentNode = this.findParent(node.data);
+      // continues to the next code blocks depending on the condition
+    }
+
+    //remove a leaf node
+    if (!hasLeftChild && !hasRightChild) {
+      const isLeftChild = parentNode && parentNode.left.data === value;
+      isLeftChild ? (parentNode.left = null) : (parentNode.right = null);
+      return;
+    }
+
+    //FIXME make sure only when it has one child it get removed
+    // remove a node when it has a single child
+    const hasChild = node.left || node.right;
+    // if (hasChild) {
+    if (parentNode.left && parentNode.left.data === value) {
+      parentNode.left = hasChild;
+      return;
+    } else {
+      parentNode.right = hasChild;
+    }
+    // }
+  }
+
   find(value, node = this.root) {
     if (node === null) return null;
     if (node.data === value) return node;
 
     const searchLeft = this.find(value, node.left);
     const searchRight = this.find(value, node.right);
+    return searchLeft || searchRight;
+  }
+
+  findSuccessor(node) {
+    let successor = node.right;
+    const hasLeftChild = node.left;
+
+    if (hasLeftChild) {
+      while (successor.left !== null) {
+        successor = successor.left;
+      }
+      return successor;
+    } else {
+      return successor;
+    }
+  }
+
+  findParent(value, node = this.root) {
+    if (node === null) return null;
+    if (node.right && node.right.data === value) return node;
+    if (node.left && node.left.data === value) return node;
+
+    const searchLeft = this.findParent(value, node.left);
+    const searchRight = this.findParent(value, node.right);
     return searchLeft || searchRight;
   }
 
@@ -77,8 +139,8 @@ class Tree {
 
 const tree = new Tree([1, 7, 4, 23, 8, 10000, 4, 3, 5, 7]);
 tree.insert(999);
-tree.insert(2);
-
+tree.insert(0);
+tree.insert(888);
+tree.remove(8);
+tree.remove(0);
 tree.prettyPrint(tree.root);
-console.log(tree.root);
-console.log(tree.find(10000));
